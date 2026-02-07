cdcd6a14f9152929927a89c05d29b278b8d955f9
Asier Larrayoz <148328956+larrayoz146585@users.noreply.github.com>
2026-02-07 16:41:20 +0100

    Update API URL to production endpoint

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 
-const API_URL = 'http://192.168.1.158:8000/api'; 
+const API_URL = 'https://pe-aapp-back.onrender.com'; 
 
 const api = axios.create({
   baseURL: API_URL,
