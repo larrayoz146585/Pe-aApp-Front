888517af9e4357a685b07c3e7f0028ad7d55e5ee
Asier Larrayoz <148328956+larrayoz146585@users.noreply.github.com>
2026-02-13 17:05:16 +0100

    Modify API URL and enhance token handling
    Updated API URL and added platform-specific token retrieval logic.

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
+import { Platform } from 'react-native'; // <--- IMPORTANTE: Importar esto
 
-const API_URL = 'https://pe-aapp-back.onrender.com'; 
+const API_URL = 'https://pena-app-back.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
@@ -11,14 +12,26 @@ const api = axios.create({
   },
 });
 
+// Interceptor para añadir el token automáticamente
 api.interceptors.request.use(async (config) => {
-  const token = await SecureStore.getItemAsync('user_token');
+  let token;
+
+  // --- LÓGICA HÍBRIDA (WEB vs MÓVIL) ---
+  if (Platform.OS === 'web') {
+    // Si estamos en el navegador, usamos localStorage
+    token = localStorage.getItem('user_token');
+  } else {
+    // Si estamos en el móvil, usamos SecureStore
+    token = await SecureStore.getItemAsync('user_token');
+  }
+  // -------------------------------------
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
 export default api;
 
-'http://192.168.1.158:8000/api'; 
