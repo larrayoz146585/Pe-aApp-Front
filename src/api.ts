1d22ed0ab573541b88e77bbdc50e90493f18f6c2
larrayoz146585 <148328956+larrayoz146585@users.noreply.github.com>
2026-02-21 16:22:24 +0100

    la url de la api estaba la local

--- src/api.ts
+++ src/api.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 import { Platform } from 'react-native';
 
-//const API_URL = 'https://pena-app-back.onrender.com/api'; 
-const API_URL = 'http://127.0.0.1:8000/api';
+const API_URL = 'https://pena-app-back.onrender.com/api';
+//const API_URL = 'http://127.0.0.1:8000/api';
 
 const api = axios.create({
   baseURL: API_URL,
